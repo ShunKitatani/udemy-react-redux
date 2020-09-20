858d7ad7a90215c4bc67ee4673e26e774c453851
shunkitatani <shun19990502@icloud.com>
2020-09-20 20:22:37 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => (<Counter></Counter>)
+
+
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {count:0}
+  }
+
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  handleminusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleminusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
-
 export default App;
